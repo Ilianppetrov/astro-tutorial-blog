6f9a46e2222f70bc4c693fcafa19c46ceda70463
Ilian Petrov <ilian.petrov@digitall.com>
2023-03-05 22:21:30 +0200

    test vercel function timeout

--- src/pages/testapi.ts
+++ src/pages/testapi.ts
@@ -1,4 +1,10 @@
 export async function get({ params }) {
+  await new Promise((resolve) =>
+    setTimeout(() => {
+      resolve("");
+    }, 11000)
+  );
+
   return new Response("success", {
     status: 200,
     headers: {
